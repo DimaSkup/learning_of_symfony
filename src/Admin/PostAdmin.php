28a9a7384abf63fd40c322e676e477c765bc0b55
Dima <DimaSkupejko@gmail.com>
2020-11-07 17:23:30 +0200

    PostAdmin #4

--- src/Admin/PostAdmin.php
+++ src/Admin/PostAdmin.php
@@ -6,6 +6,7 @@ namespace App\Admin;
 use App\Entity\Post;
 use App\Entity\User;
 use Cocur\Slugify\Slugify;
+use App\Service\FileHandleService;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
@@ -13,20 +14,25 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\CallbackTransformer;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 
 final class PostAdmin extends AbstractAdmin
 {
-    public function __construct($code, $class, $baseControllerName, Slugify $slugify)
+    public function __construct($code, $class, $baseControllerName, Slugify $slugify, FileHandleService $fileHandleService)
     {
         parent::__construct($code, $class, $baseControllerName);
         $this->slugify = $slugify;
+        $this->fileHandleService = $fileHandleService;
     }
 
     public function toString($object)
@@ -77,8 +83,32 @@ final class PostAdmin extends AbstractAdmin
         ->end()
         ;
 
+        $dataCollector = $formMapper->getFormBuilder()->get('title')->getAttribute('data_collector/passed_options');
+        $fieldDescription = $dataCollector["sonata_field_description"];
+        $postSubject = $fieldDescription->getAdmin()->getSubject();
+
+        $container = $this->getConfigurationPool()->getContainer();
 
 
+        $imageFilename =
+        dd($postSubject->getBrochureFilename());
+        //dd($formMapper->get('image'));
+
+        /*
+        $formMapper->get('image')
+            ->addModelTransformer(new CallbackTransformer(
+                function($image)
+                {
+                    return;
+                },
+                function ($image)
+                {
+                    dd($image);
+
+
+                }
+            ));
+        */
 
     }
 
@@ -103,28 +133,75 @@ final class PostAdmin extends AbstractAdmin
 
     public function prePersist($post)
     {
+        $fileHandler = $this->fileHandleService;
         $files = $this->getRequest()->files;
         $uniqid = $files->keys()[0];
         $filesParams = $files->all()[$uniqid];
 
+        // A FILES HANDLING
+        foreach ($filesParams as $key => $file)
+        {
+            /** @var UploadedFile $file */
+
+            if ($key == "brochure")
+            {
+                $fileHandler->handleBrochureFile($post, $file, 'brochures_directory');
+            }
+            else if ($key == "image")
+            {
+                $fileHandler->handleImageFile($post, $file, 'images_directory');
+            }
+            else
+            {
+                return new Response("Error! Unknown type of the file!");
+            }
+        }
+
+        $post->setSlug($this->slugify->slugify(substr($post->getTitle(), 0, 20)));
+        $post->setIsModerated(true);
+        $post->setCreatedAt(new \DateTime());
+    }
+
+    public function preUpdate($post)
+    {
+        dd("KEK");
+        $fileHandler = $this->fileHandleService;
+        $files = $this->getRequest()->files;
+        $uniqid = $files->keys()[0];
+        $filesParams = $files->all()[$uniqid];
 
         // A FILES HANDLING
         foreach ($filesParams as $key => $file)
         {
-            dd($file);
+            /** @var UploadedFile $file */
+
+            if ($key == "brochure")
+            {
+                $post->setBrochureFilename(null);
+                $fileHandler->handleBrochureFile($post, $file, 'brochures_directory');
+            }
+            else if ($key == "image")
+            {
+                $post->setImageFilename(null);
+                $fileHandler->handleImageFile($post, $file, 'images_directory');
+            }
+            else
+            {
+                return new Response("Error! Unknown type of the file!");
+            }
         }
 
-
         $post->setSlug($this->slugify->slugify(substr($post->getTitle(), 0, 20)));
-        $post->setIsModerated(true);
-
-        //$post->setImageFilename(null);
-        //$post->setBrochureFilename(null);
-        $post->setCreatedAt(new \DateTime());
-
-        dd($post);
+        //$post->setIsModerated(true);
+        //$post->setCreatedAt(new \DateTime());
     }
 
     /** @var Slugify */
     private $slugify;
+
+    /** @var FileHandleService */
+    private $fileHandleService;
+
+    /** @var ParameterBagInterface */
+    $parameterBag;
 }
